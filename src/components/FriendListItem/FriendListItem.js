b666992a6e11fd83ca3c7ba0cdaa12de7b77efb7
ANDRII ALENIN <mkalenin@gmail.com>
2021-06-07 17:35:17 +0530

    Update FriendListItem.js

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -2,12 +2,17 @@ import styles from "./FriendListItem.module.css"
 
 
 
-const FriendListItem = ({ avatar, name, isOnline, id }) => (
-  <li className={styles.FriendListItem} key={id}>
-    <span className={isOnline ? styles.Online : styles.Offline}></span>
-    <img className="avatar" src={avatar} alt={name} width='30' />
-    <p className="name">{name}</p>
-  </li>
+const FriendListItem = ({ avatar, name, isOnline, id }) => ( <
+    li className = { styles.FriendListItem } >
+    <
+    span className = { isOnline ? styles.Online : styles.Offline } > < /span> <
+    img className = "avatar"
+    src = { avatar }
+    alt = { name }
+    width = '30' / >
+    <
+    p className = "name" > { name } < /p> < /
+    li >
 )
 
 export default FriendListItem
