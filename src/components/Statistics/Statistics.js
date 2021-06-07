2291b478955244e19a7c0519e27a1274434cfd3b
ANDRII ALENIN <mkalenin@gmail.com>
2021-06-07 17:43:39 +0530

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,27 +2,32 @@ import PropTypes from "prop-types";
 
 import styles from "./Statistics.module.css"
 
-const Statistics = ({stats}) =>
-   <section>
-  <h2 className="statistics-title">Upload stats</h2>
-  <ul className={styles.StatisticsList}> 
-      {stats.map(item =>
-      <li key={item.id} className={styles.StatisticsItem}>
-          <span>{ item.label}: </span>
-          <span>{ item.percentage}%</span>
-        </li>
-      )}
-  </ul>
-</section>
+const Statistics = ({ stats }) =>
+    <
+    section > { /* <h2 className="statistics-title">Upload stats</h2> */ } {
+        title && < h2 className = "statistics-title" > Upload stats > { title } < /h2>} <
+        ul className = { styles.StatisticsList } > {
+                stats.map(item =>
+                    <
+                    li key = { item.id }
+                    className = { styles.StatisticsItem } >
+                    <
+                    span > { item.label }: < /span> <
+                    span > { item.percentage } % < /span> < /
+                    li >
+                )
+            } <
+            /ul> < /
+        section >
 
 
-Statistics.propTypes = {
-  stats: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    }),
-  ),
-};
-export default Statistics
+            Statistics.propTypes = {
+                stats: PropTypes.arrayOf(
+                    PropTypes.shape({
+                        id: PropTypes.string.isRequired,
+                        label: PropTypes.string.isRequired,
+                        percentage: PropTypes.number.isRequired,
+                    }),
+                ),
+            };
+        export default Statistics
