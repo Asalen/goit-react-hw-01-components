1852b0e88c5ce0ba2fea4cd6833f534611a49fbd
ANDRII ALENIN <mkalenin@gmail.com>
2021-06-07 17:33:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import FriendsList from './components/FriendsList/FriendsList'
 import TransactionHistory from './components/TransactionHistory/TransactionHistory'
 import Container from './components/Container/Container'
 
-import {avatar, name, tag, location, stats} from './data/user.json'
+import { avatar, name, tag, location, stats } from './data/user.json'
 import statisticsData from './data/statistical-data.json'
 import friendsData from './data/friends.json'
 import transactionsData from './data/transaction.json'
 
-const App = () => (
-  <Container>
-    <UserCard
-      avatar={avatar}
-      alt={name}
-      name={name}
-      tag={tag}
-      location={location}
-      stats={stats} />
-    <Statistics stats={statisticsData} />
-    <FriendsList friends={friendsData} />
-    <TransactionHistory transactions={transactionsData} />
-  </ Container>
+const App = () => ( <
+    Container >
+    <
+    UserCard avatar = { avatar }
+    name = { name }
+    tag = { tag }
+    location = { location }
+    stats = { stats }
+    /> <
+    Statistics stats = { statisticsData }
+    /> <
+    FriendsList friends = { friendsData }
+    /> <
+    TransactionHistory transactions = { transactionsData }
+    /> < /
+    Container >
 );
 
 export default App;
